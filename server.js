2f56b443830afdbb2010c52db6487096f6cba74b
MRauhe <martrauhe@hotmail.com>
2017-11-05 07:29:00 -0800

    Test

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-var express = require('express');
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
-	
-server.listen(process.env.PORT || 3000);
+var http = require('http'),
+	express = require('express'),
+	chatServer = require('./lib/chat-server');
 
-app.get('/', function(req, res) {
-	res.sendfile(__dirname + '/index.html');
-});
+var app = express();
+app.use(app.router);
+app.use(express.static(__dirname + '/public'));
 
-io.sockets.on('connection', function(socket){
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', data);
-	});
+var server = http.createServer(app).listen(process.env.PORT || 3000); 
+chatServer.listen(server);
+
+app.get('/', function(req, res){
+	res.sendfile(__dirname + '/views/index.html');
 });
